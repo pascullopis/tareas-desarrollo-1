3b7fa60ba1351064576d37883fc4fb9662406fe9
Pascu <pascu.llopis@gmail.com>
2015-10-18 17:58:17 +0200

    Update td1.py

--- td1.py
+++ td1.py
@@ -1,17 +1,2 @@
 # -*- coding: utf-8 -*-
 
-print ""
-print ""
-print "IMPRIMIR PARÁMETROS DE ENTRADA DE EJECUCIÓN"
-print ""
-print "se ejecuta así:   $ python exParametros.py parametro1 parametro2 parametro3"
-print ""
-
-from sys import argv
-
-script, first, second, third = argv
-
-print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is:", second
-print "Your third variable is:", third
